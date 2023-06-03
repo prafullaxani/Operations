38180a23f6a0d1f41b6df3813e5e7b83046e7f37
prafullaxani <prafullaxani@gmail.com>
2023-06-03 11:35:36 +0530

    my practice commite

---
+++ src/Operations.java
@@ -0,0 +1,34 @@
+
+public class Operations 
+{
+ static int a=10;
+ static int b=20;
+ 
+ public static void add()
+ {
+	 int res=a+b;
+	 System.out.println(res);
+ }
+ public static void sub()
+ {
+	 int res1=b-a;
+	 System.out.println(res1);
+ }
+ public static void mul()
+ {
+	 int res3=a*b;
+	 System.out.println(res3);
+ }
+ public static void div()
+ {
+	 int res4=b/a;
+	 System.out.println(res4);
+ }
+ public static void main(String[] args) 
+ {
+	add();
+	sub();
+	mul();
+	div();
+ }
+}
